3944fd346dd4c18cfff2c8efe3e727fd0404c29e
Muhammad Zaka ILLAHi <babrumbabrum12@gmail.com>
2026-02-24 21:42:24 +0500

    Setup iterative multiplication table generator
    "Starting the project for a table generator. The program asks the user for a number and then uses a for loop to multiply that number by 1 through 10, showing the full multiplication table in the console."

---
+++ Multiplication_Table_Fo_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int num=0;
+	
+	
+	cout<<"Enter number: ";
+	cin>>num;
+	
+	
+	for(int i=1;i<=10;i++)
+	{
+		cout<<num<<"x"<<i<<"="<<num*i<<endl;		
+	}
+}
